f92014aa74ddffe0ee79596f27e41274565964ab
Huan Wang <huan.w.wang@gmail.com>
2020-07-10 14:00:56 -0400

    initial push with one constructor

---
+++ classwork/08/Stack.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+// 
+
+class Stack {
+	
+	private int length;
+	private boolean isFront;
+	private String data;
+	
+	public Stack(){
+	
+		data = null;
+		length = 0;
+		isFront = true;
+	
+	}
+	
+	
+}
