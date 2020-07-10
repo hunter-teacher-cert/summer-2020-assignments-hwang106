e2293bbf1d3236380fad432405ce67d98ebb939b
Huan Wang <huan.w.wang@gmail.com>
2020-07-10 16:12:51 -0400

    Stopped being lazy and made a driver

---
+++ classwork/08/StackDriver.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class StackDriver{
+	
+	public static void main(String[] args){
+		
+		Stack s1 = new Stack();
+		System.out.println("This is what the initialized empty stack looks like \n" + s1);
+		System.out.println("Is this stack empty? \n" + s1.isEmpty());
+		System.out.println("This stack has a size of " + s1.getLength());
+		
+		//Testing exception thrown if main attempts to pop off empty stack
+		
+		try {
+			s1.pop();
+		}
+		
+		catch (NoSuchElementException e){
+			System.out.println("What's poppin'? Nothing! You can't pop off an empty stack!");
+		}
+		
+		
+		s1.push("bottom");
+		System.out.println("This is what the stack looks like after pushing one element \n" + s1);
+		
+		s1.push("middle");
+		System.out.println("This is what the stack looks like after pushing two elements \n" + s1);
+		
+		s1.push("top");
+		System.out.println("This is what the stack looks like after pushing three elements \n" + s1);
+		
+		System.out.println("Is this stack empty? \n" + s1.isEmpty());
+		System.out.println("This stack has a size of " + s1.getLength());
+		System.out.println("The top of this stack returns " + s1.top());
+		
+		s1.setTop("new top");
+		System.out.println("Is this stack empty? \n" + s1.isEmpty());
+		System.out.println("This stack has a size of " + s1.getLength());
+		System.out.println("The top of this stack returns " + s1.top());
+		
+		s1.pop();
+		System.out.println("This is what the stack looks like after invoking pop once \n" + s1);
+		s1.pop();
+		System.out.println("This is what the stack looks like after invoking pop twice \n" + s1);
+		s1.pop();
+		System.out.println("This is what the stack looks like after invoking pop three times \n" + s1);
+		s1.pop(); //this should cause an exception
+		
+		
+		
+		
+	}
+	
+	
+}
